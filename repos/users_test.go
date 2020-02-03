0e7f7af96f89f43b7223293d3b2e3522a1931518
jimmy <tv2169145@gmail.com>
2020-02-03 23:32:06 +0800

    userRepo add findById and findByEmail unit test

--- repos/users_test.go
+++ repos/users_test.go
@@ -13,7 +13,6 @@ import (
 
 var _ = Describe("UsersRepo", func() {
 	var (
-		err error
 		usr *User
 
 		setupData = func() {
@@ -37,13 +36,13 @@ var _ = Describe("UsersRepo", func() {
 		Context("Failures", func() {
 			// usr equal nil
 			It("should fail with a nil user", func() {
-				err = gr.Users().Create(nil)
+				err := gr.Users().Create(nil)
 				Ω(err).NotTo(BeNil())
 				Ω(err.Error()).To(Equal("validator: (nil *types.User)"))
 			})
 			// Invalid user data
 			It("should fail with a bad user", func() {
-				err = gr.Users().Create(&types.User{
+				err := gr.Users().Create(&types.User{
 					Password: usr.Password,
 					Visible: true,
 
@@ -58,7 +57,7 @@ var _ = Describe("UsersRepo", func() {
 					WithArgs(usr.FirstName, usr.LastName, usr.Email, usr.Password, usr.Visible).
 					WillReturnError(errors.New(errMsg))
 
-				err = gr.Users().Create(usr)
+				err := gr.Users().Create(usr)
 				Ω(err).NotTo(BeNil())
 				Ω(err.Error()).To(Equal(errMsg))
 			})
@@ -69,10 +68,92 @@ var _ = Describe("UsersRepo", func() {
 					WithArgs(usr.FirstName, usr.LastName, usr.Email, usr.Password, usr.Visible).
 					WillReturnResult(sqlmock.NewResult(1, 1))
 
-				err = gr.Users().Create(usr)
+				err := gr.Users().Create(usr)
 				Ω(err).To(BeNil())
 			})
 		})
+	})
 
+	Describe("FindById", func() {
+		Context("Failures", func() {
+			It("should fail with a bad id", func() {
+				_, err := gr.Users().FindById(0)
+				Ω(err).NotTo(BeNil())
+				Ω(err.Error()).To(Equal("invalid user id"))
+			})
+			It("should fail with database error", func() {
+				usr.ID = 1
+				errMsg := "database unavailable"
+				mock.ExpectQuery("SELECT `id`, `first_name`, `last_name`, `email`, `password`, `visible` FROM `users` WHERE `id`=? LIMIT 1").
+					WithArgs(usr.ID).
+					WillReturnError(errors.New(errMsg))
+				_, err := gr.Users().FindById(usr.ID)
+				Ω(err).NotTo(BeNil())
+				Ω(err.Error()).To(Equal(errMsg))
+			})
+			It("should fail with not found a user", func() {
+				errMsg := "unable to find a user"
+				usr.ID = 1
+				mock.ExpectQuery("SELECT `id`, `first_name`, `last_name`, `email`, `password`, `visible` FROM `users` WHERE `id`=? LIMIT 1").
+					WithArgs(usr.ID).
+					WillReturnRows(sqlmock.NewRows([]string{}))
+				_, err := gr.Users().FindById(usr.ID)
+				Ω(err).NotTo(BeNil())
+				Ω(err.Error()).To(Equal(errMsg))
+			})
+		})
+
+		Context("success", func() {
+			It("should successfully find a user", func() {
+				usr.ID = 1
+				mock.ExpectQuery("SELECT `id`, `first_name`, `last_name`, `email`, `password`, `visible` FROM `users` WHERE `id`=? LIMIT 1").
+					WithArgs(usr.ID).
+					WillReturnRows(sqlmock.NewRows(
+						[]string{"id", "first_name", "last_name", "email", "password", "visible"},
+					).AddRow(usr.ID, usr.FirstName, usr.LastName, usr.Email, usr.Password, usr.Visible))
+				foundUser, err := gr.Users().FindById(usr.ID)
+				Ω(err).To(BeNil())
+				Ω(foundUser).To(BeEquivalentTo(usr))
+			})
+		})
+	})
+
+	Describe("FindByEmail", func() {
+		Context("failures", func() {
+			It("should fail with empty email", func() {
+				_, err := gr.Users().FindByEmail("")
+				Ω(err).NotTo(BeNil())
+				Ω(err.Error()).To(Equal("invalid user email"))
+			})
+			It("should fail with database error", func() {
+				errMsg := "database unavailable"
+				mock.ExpectQuery("SELECT `id`, `first_name`, `last_name`, `email`, `password`, `visible` FROM `users` WHERE `email`=? LIMIT 1").
+					WithArgs(usr.Email).
+					WillReturnError(errors.New(errMsg))
+				_, err := gr.Users().FindByEmail(usr.Email)
+				Ω(err).NotTo(BeNil())
+				Ω(err.Error()).To(Equal(errMsg))
+			})
+			It("should fail with not found a user", func() {
+				errMsg := "unable to find a user"
+				mock.ExpectQuery("SELECT `id`, `first_name`, `last_name`, `email`, `password`, `visible` FROM `users` WHERE `email`=? LIMIT 1").
+					WithArgs(usr.Email).
+					WillReturnRows(sqlmock.NewRows([]string{}))
+				_, err := gr.Users().FindByEmail(usr.Email)
+				Ω(err).NotTo(BeNil())
+				Ω(err.Error()).To(Equal(errMsg))
+			})
+		})
+
+		Context("success", func() {
+			It("should successfully find a user", func() {
+				mock.ExpectQuery("SELECT `id`, `first_name`, `last_name`, `email`, `password`, `visible` FROM `users` WHERE `email`=? LIMIT 1").
+					WithArgs(usr.Email).
+					WillReturnRows(sqlmock.NewRows([]string{"id", "first_name", "last_name", "email", "password", "visible"}).AddRow(usr.ID, usr.FirstName, usr.LastName, usr.Email, usr.Password, usr.Visible))
+				foundUser, err := gr.Users().FindByEmail(usr.Email)
+				Ω(err).To(BeNil())
+				Ω(foundUser).To(BeEquivalentTo(usr))
+			})
+		})
 	})
 })
